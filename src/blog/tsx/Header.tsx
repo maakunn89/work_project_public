b56ef638ae3e47aa8165feb88f694e89abb72919
Nakazato-dreamcareer <masaaki.nakazato@g.dreamcareer.co.jp>
2024-07-31 11:37:21 +0900

    headerの雛形を作成

--- src/blog/tsx/Header.tsx
+++ src/blog/tsx/Header.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../css/Header.css'
 
 const Header = () => {
-  return <h2>Header</h2>
+  return (
+    <div className="header_content">
+      <div className="header_title">Header</div>
+      <div className="header_links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/works">Works</Link>
+        <Link to="/links">Links</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Header
