07faf349103fcc5114b21d07acc0a8ed1b77ef62
maakunn89 <maakunn89@yahoo.co.jp>
2024-08-02 16:11:10 +0900

    各ページの背景画像の設定を中心に、レイアウト修正

--- src/blog/tsx/Notfound.tsx
+++ src/blog/tsx/Notfound.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../css/Works.css'
+import '../css/Notfound.css'
 import { Helmet } from 'react-helmet-async'
 
 const Notfound = () => {
   const componentName = 'Not found'
   return (
     <div className="content_wrapper">
-      <Helmet>
-        <title>Principal Dawn | {componentName}</title>
-      </Helmet>
-      <div className="page_title">
-        Not found
-        <p className="title_description">ページが見つかりませんでした</p>
-      </div>
-      <div className="contents text">
-        <div>お手数ですが、URLをお確かめの上再度アクセスするか、</div>
-        <div>
-          <Link to="/" className="link">
-            トップへ
-          </Link>
-          お戻りください。
+      <div className="notfound_img">
+        <Helmet>
+          <title>Principal Dawn | {componentName}</title>
+        </Helmet>
+        <div className="notfound_bg">
+          <div className="page_title">
+            Not found
+            <p className="title_description">ページが見つかりませんでした</p>
+          </div>
+          <div className="contents text">
+            <div>お手数ですが、URLをお確かめの上再度アクセスするか、</div>
+            <div>
+              <Link to="/" className="link">
+                トップへ
+              </Link>
+              お戻りください。
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/blog/tsx/Secret.tsx
+++ src/blog/tsx/Secret.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import '../css/About.css'
+import '../css/Secret.css'
 import { Helmet } from 'react-helmet-async'
 
 const Secret = () => {
   const componentName = 'Secret'
   return (
     <div className="content_wrapper">
-      <Helmet>
-        <title>Principal Dawn | {componentName}</title>
-      </Helmet>
-      <div className="page_title">
-        Secret
-        <p className="title_description">みんなにはないしょだよ</p>
-      </div>
-      <div className="contents text">
-        <div>何を入れるかは未定です　とりあえず見つけた君はすごい</div>
+      <div className="secret_img">
+        <Helmet>
+          <title>Principal Dawn | {componentName}</title>
+        </Helmet>
+        <div className="secret_bg">
+          <div className="page_title">
+            Secret
+            <p className="title_description">みんなにはないしょだよ</p>
+          </div>
+          <div className="contents text">
+            <div>何を入れるかは未定です　とりあえず見つけた君はすごい</div>
+          </div>
+        </div>
       </div>
     </div>
   )
